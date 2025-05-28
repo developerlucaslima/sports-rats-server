50194ef9d2cd304f37aae00a6ccb919b3cfa8f48
developerlucaslima <developerlucaslima@gmail.com>
2025-05-28 13:25:31 -0300

    feat: add PlayersModule and SharedModule to AppModule imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PlayersModule } from './players/players.module';
+import { SharedModule } from './shared/shared.module';
 
 @Module({
-  imports: [],
+  imports: [PlayersModule, SharedModule],
   controllers: [AppController],
   providers: [AppService],
 })
