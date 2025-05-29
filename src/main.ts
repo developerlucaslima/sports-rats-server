fc728e9c3e048e0ecdb2c907c1d30cee58ddef0d
developerlucaslima <developerlucaslima@gmail.com>
2025-05-29 15:50:30 -0300

    refactor: simplify bootstrap function by replacing ConfigService with EnvService

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,12 @@
-/* eslint-disable @typescript-eslint/no-unsafe-argument */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/no-floating-promises */
-import type { Env } from '@env/env'
-import { ConfigService } from '@nestjs/config'
+import { EnvService } from '@config/env/env.service'
 import { NestFactory } from '@nestjs/core'
 
 import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  const configService = app.get<ConfigService<Env, true>>(ConfigService)
-  const port = configService.get('PORT', { infer: true })
+  const configService = app.get(EnvService)
+  const port = configService.get('PORT')
   await app.listen(port)
 }
 bootstrap()
