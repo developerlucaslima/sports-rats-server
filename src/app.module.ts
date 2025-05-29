f7aa61c07188e03c407f38291df4dfb4c3eed416
developerlucaslima <developerlucaslima@gmail.com>
2025-05-29 03:11:12 -0300

    feat: add EnvModule to AppModule imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
-import { Module } from '@nestjs/common';
-import { PlayersModule } from './players/players.module';
-import { SharedModule } from './shared/shared.module';
+import { EnvModule } from '@env/env.module'
+import { Module } from '@nestjs/common'
+import { PlayersModule } from '@players/players.module'
+import { SharedModule } from '@shared/shared.module'
 
 @Module({
-  imports: [PlayersModule, SharedModule],
+  imports: [PlayersModule, SharedModule, EnvModule],
 })
 export class AppModule {}
