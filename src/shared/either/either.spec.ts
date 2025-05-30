c3ef034c5c6888f6b23b564f236f9cf15639ed9d
developerlucaslima <developerlucaslima@gmail.com>
2025-05-30 00:29:34 -0300

    feat: implement Either type with Success and Failure classes

---
+++ src/shared/either/either.spec.ts
@@ -0,0 +1,23 @@
+import { type Either, failure, success } from './either'
+
+function doSomeThing(shouldSuccess: boolean): Either<string, number> {
+  if (shouldSuccess) {
+    return success(10)
+  } else {
+    return failure('error')
+  }
+}
+
+test('success result', () => {
+  const result = doSomeThing(true)
+
+  expect(result.isSuccess()).toBe(true)
+  expect(result.isFailure()).toBe(false)
+})
+
+test('failure result', () => {
+  const result = doSomeThing(false)
+
+  expect(result.isFailure()).toBe(true)
+  expect(result.isSuccess()).toBe(false)
+})
