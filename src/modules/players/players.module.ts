03831a9e1123aaaf2b44347cea1d4724b413c4cd
developerlucaslima <developerlucaslima@gmail.com>
2025-06-04 01:23:25 -0300

    feat: configure PlayersModule to use PrismaPlayersRepository for player data operations

--- src/modules/players/players.module.ts
+++ src/modules/players/players.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common'
 
+import { PlayersRepository } from './domain/repositories/players-repository'
+import { PrismaPlayersRepository } from './infrastructure/prisma/repositories/prisma-players-repository'
+
 @Module({
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: PlayersRepository,
+      useClass: PrismaPlayersRepository,
+    },
+  ],
+  exports: [PlayersRepository],
 })
 export class PlayersModule {}
