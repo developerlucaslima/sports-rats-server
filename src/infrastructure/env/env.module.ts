e72232cc631692e3208ce0592b87902f4cd6d24c
developerlucaslima <developerlucaslima@gmail.com>
2025-06-04 01:24:09 -0300

    feat: add EnvModule and EnvService for environment configuration management

---
+++ src/infrastructure/env/env.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+
+import { envSchema } from './env'
+import { EnvService } from './env.service'
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+      cache: true,
+    }),
+  ],
+  providers: [EnvService],
+  exports: [EnvService],
+})
+export class EnvModule {}
